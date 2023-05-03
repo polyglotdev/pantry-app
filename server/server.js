7d38cd33b603b0f2650eec61f7b2c0aa8d720261
Evan Donica <cevandonica@gmail.com>
2023-05-03 09:49:32 -0500

    Took out my password

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const serverapp = express();
+
+const PASSWORD_NAME = process.env.PASSWORD_NAME;
+
 
 serverapp.use(cors());
 serverapp.use(express.json());
 
-mongoose.connect('mongodb+srv://cevandonica:TJxAYqNTxOwer1ld@cluster0.hfturxs.mongodb.net/test', {
+mongoose.connect('mongodb+srv://cevandonica:'+PASSWORD_NAME+'@cluster0.hfturxs.mongodb.net/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });

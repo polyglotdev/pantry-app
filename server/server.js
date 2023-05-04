47fac2aae6af92597a6a639aa91f46635b5320b0
Evan Donica <cevandonica@gmail.com>
2023-05-03 22:54:06 -0500

    added server code

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://cevandonica:'+process.env.PASSWORD_NAME+'@cluster0.hfturxs.mongodb.net/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch(error => {
+    console.error(error);
+  });
+;
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});

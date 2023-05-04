4076138770b5a3529928456d77abbdd49b6bf7a9
Dom Hallan <dom.hallan@hashicorp.com>
2023-05-04 18:57:12 -0500

    🎆 add config

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD_NAME}@cluster0.hfturxs.mongodb.net/test`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-    console.log('Connected to MongoDB Atlas');
-  })
+  console.log('Connected to MongoDB Atlas')
+})
   .catch(error => {
-    console.error(error);
-  });
-;
+    console.error(error)
+  })
 
-const port = process.env.PORT || 3001;
+
+const port = process.env.PORT || 3001
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  console.log(`🚀 listening on port ${port}`)
+})

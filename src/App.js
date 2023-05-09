1985ffd79c00c8860c5e00d7f950ba190de6ce85
Dom Hallan <dom@polyglotdev.com>
2023-05-08 20:22:35 -0500

    this is a test for frontend folks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import NavBar from './Navbar'
 import Main from './Main'
+import SignIn from './SignIn'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Main />
+      <SignIn />
     </div>
   )
 }

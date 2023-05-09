6f4c56f56d7a7b79dc768f35a33723d7a2629806
Evan Donica <cevandonica@gmail.com>
2023-05-08 22:30:42 -0500

    Removed test variable

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,63 +2,44 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const { UserModel } = require('../models/Users');
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const router = express.Router();
-
-// Register Route
 router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
 
-  // Validate input
-  if (!username || !password) {
-    return res.status(400).json({ error: 'Username and password are required' });
-  }
+    if (user) {
+        return res.status(400).json({ error: 'Username already exists' });
+    }
 
-  // Sanitize input
-  const cleanUsername = username.replace(/[^a-zA-Z0-9]/g, '');
-  const cleanPassword = password.replace(/[^a-zA-Z0-9]/g, '');
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User created' });
 
-  const user = await UserModel.findOne({ username: cleanUsername });
-
-  if (user) {
-    return res.status(400).json({ error: 'Username already exists' });
-  }
-
-  const hashedPassword = await bcrypt.hash(cleanPassword, 10);
-  const newUser = new UserModel({ username: cleanUsername, password: hashedPassword });
-  await newUser.save();
-  res.status(201).json({ message: 'User created' });
 });
 
-// Login Route
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;   
+    const user = await UserModel.findOne({ username });
+    
+    if (!user) {
+        return res.status(400).json({ error: 'Invalid username or password' });
+    }
 
-  // Validate input
-  if (!username || !password) {
-    return res.status(400).json({ error: 'Username and password are required' });
-  }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-  // Sanitize input
-  const cleanUsername = username.replace(/[^a-zA-Z0-9]/g, '');
-  const cleanPassword = password.replace(/[^a-zA-Z0-9]/g, '');
+    if(!isPasswordValid) {
+        return res.status(400).json({ error: 'Invalid username or password' });
+    }
+    
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ token, userID: user._id });
+})
 
-  const user = await UserModel.findOne({ username: cleanUsername });
-
-  if (!user) {
-    return res.status(400).json({ error: 'Invalid username or password' });
-  }
-
-  const isPasswordValid = await bcrypt.compare(cleanPassword, user.password);
-
-  if (!isPasswordValid) {
-    return res.status(400).json({ error: 'Invalid username or password' });
-  }
-
-  const token = jwt.sign({ id: user._id }, "secret");
-  res.json({ token, userID: user._id });
-});
-
-module.exports = router; 
+module.exports = router;   
 
 

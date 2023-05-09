4998e1d25b4259c5922a412b9187cde5ebfc8e5d
Evan Donica <cevandonica@gmail.com>
2023-05-08 21:21:57 -0500

    added sanitation

--- src/models/Items.js
+++ src/models/Items.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    expirationDate: {type: Date, required: true},
+    quantity: {type: Number, required: true},
+    location: {type: String, required: true},
+    foodGroup: {type: String, required: true}
+});
+
+module.exports = mongoose.model('ItemModel', ItemSchema);
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,23 +4,61 @@ const { UserModel } = require('../models/Users');
 const bcrypt = require('bcrypt');
 
 const router = express.Router();
+
+// Register Route
 router.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await UserModel.findOne({ username });
+  const { username, password } = req.body;
 
-    if (user) {
-        return res.status(400).json({ error: 'Username already exists' });
-    }
+  // Validate input
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new UserModel({ username, password: hashedPassword });
-    await newUser.save();
-    res.status(201).json({ message: 'User created' });
+  // Sanitize input
+  const cleanUsername = username.replace(/[^a-zA-Z0-9]/g, '');
+  const cleanPassword = password.replace(/[^a-zA-Z0-9]/g, '');
 
+  const user = await UserModel.findOne({ username: cleanUsername });
+
+  if (user) {
+    return res.status(400).json({ error: 'Username already exists' });
+  }
+
+  const hashedPassword = await bcrypt.hash(cleanPassword, 10);
+  const newUser = new UserModel({ username: cleanUsername, password: hashedPassword });
+  await newUser.save();
+  res.status(201).json({ message: 'User created' });
 });
 
-router.post('/login', async (req, res) => {})
+// Login Route
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
 
-module.exports = router;   
+  // Validate input
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+
+  // Sanitize input
+  const cleanUsername = username.replace(/[^a-zA-Z0-9]/g, '');
+  const cleanPassword = password.replace(/[^a-zA-Z0-9]/g, '');
+
+  const user = await UserModel.findOne({ username: cleanUsername });
+
+  if (!user) {
+    return res.status(400).json({ error: 'Invalid username or password' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(cleanPassword, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(400).json({ error: 'Invalid username or password' });
+  }
+
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
+});
+
+module.exports = router; 
 
 

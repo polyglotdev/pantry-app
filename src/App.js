53dbde02794a00e53d9fec28b558d0dd5ba54657
Robyn <115902450+robynray@users.noreply.github.com>
2023-05-11 19:31:23 -0500

    My Pantry Home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import NavBar from './Navbar'
 import Main from './Main'
+import MyPantryHome from './MyPantryHome'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Main />
+      <MyPantryHome />
     </div>
   )
 }

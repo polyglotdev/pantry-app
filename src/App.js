88a07a136bfc231177d1c252da7080ca50b942c2
buijennyh <109253137+buijennyh@users.noreply.github.com>
2023-05-15 20:40:59 -0500

    added expiration date options to sign up page and profile

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './App.css'
 import NavBar from './Navbar'
 import Main from './Main'
-import SignUpForm from './SignUpForm'
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Main />
-      <SignUpForm />
+
     </div>
   )
 }

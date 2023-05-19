c75e2b5956b5723e828c4890d11811b694c71f51
redbirdfan <redbirdfan38@gmail.com>
2023-05-18 19:34:54 -0500

    really this time

--- src/Pages/Pantry.js
+++ src/Pages/Pantry.js
@@ -1,26 +1,14 @@
 import React from "react";
 import NavBar from "../Navbar";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 import MyPantryHome from "../MyPantryHome"
->>>>>>> main
-=======
-import MyPantryHome from "../MyPantryHome"
->>>>>>> main
+
 
 function Pantry() {
   return <div className="App">
     <NavBar />
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
     <MyPantryHome />
->>>>>>> main
-=======
-    <MyPantryHome />
->>>>>>> main
-    <h1>Pantry Page</h1>
 </div>
 }
 

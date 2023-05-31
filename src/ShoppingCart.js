d949b4b7ac27d8d58485996deb9d3c4d37725949
Evan Donica <cevandonica@gmail.com>
2023-05-30 19:35:08 -0500

    Reworked the shopping list and added data

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,30 +1,48 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/20/solid'
 import React from 'react'
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
-
-const products = [
-  {
-    id: 1,
-    name: 'Apples- Honey Crisp',
-    href: '#',
-    itemTotal: '4',
-    inStock: true,
-    },
-  {
-    id: 2,
-    name: 'Zucchini',
-    href: '#',
-    itemTotal: '1',
-    inStock: false,
-
-  },
-  // More products...
-]
+import axios from 'axios'
 
 export default function Example() {
+  const [products, setProducts] = useState([]);
+  const [quantityToAdd, setQuantityToAdd] = useState(1);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/item/inventory');
+        const formattedProducts = response.data
+        .filter((item) => item.restock === true)
+        .map((item) => ({
+          id: item._id.$oid,
+          name: item.name,
+          href: `#`,
+          expiration: formatDate(item.expirationDate),
+          quantity: item.quantity,
+          unit: item.unit,
+          inStock: item.quantity > 0,
+          isExpiringSoon: item.expirationDate < item.alertDate,
+        }));
+        setProducts(formattedProducts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+  };
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-4xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
@@ -52,7 +70,9 @@ export default function Example() {
                           </h3>
                         </div>
 
-                        <p className="text-right text-sm font-medium text-gray-900">{product.itemTotal}</p>
+                        <p className="text-right text-sm font-medium text-gray-900">
+                          Expiration Date: {product.expiration}
+                        </p>
                       </div>
 
                       <div className="mt-4 flex items-center sm:absolute sm:left-1/2 sm:top-0 sm:mt-0 sm:block">
@@ -75,12 +95,9 @@ export default function Example() {
                           {/* need to add 1-100 or a text input box */}
                         </select>
 
-                        <button
-                          type="button"
-                          className="ml-4 text-sm font-medium text-indigo-600 hover:text-indigo-500 sm:ml-0 sm:mt-3"
-                        >
-                          <span>Add to List</span>
-                        </button>
+                        <p className="ml-4 text-sm font-medium text-gray-600 sm:ml-0 sm:mt-3">
+                          <span>Amount</span>
+                        </p>
                       </div>
                     </div>
 
@@ -88,10 +105,14 @@ export default function Example() {
                       {product.inStock ? (
                         <CheckIcon className="h-5 w-5 flex-shrink-0 text-green-500" aria-hidden="true" />
                       ) : (
-                        <ClockIcon className="h-5 w-5 flex-shrink-0 text-gray-300" aria-hidden="true" />
+                        <XMarkIcon className="h-5 w-5 flex-shrink-0 text-red-500" aria-hidden="true" />
                       )}
 
-                      <span>{product.inStock ? 'Currently in your pantry' : `Not in your pantry`}</span>
+                      {product.inStock ? (
+                        <span>{`You currently have ${product.quantity} ${product.unit} in stock`}</span>
+                      ) : (
+                        <span>Not in your pantry</span>
+                      )}
                     </p>
                   </div>
                 </li>

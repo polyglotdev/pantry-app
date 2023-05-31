90a75bc73c1a36c6ff6f4364219839c022d6ea5a
buijennyh <109253137+buijennyh@users.noreply.github.com>
2023-05-31 08:21:42 -0500

    added component GenerateShoppingList

--- src/Pages/StartShopping.js
+++ src/Pages/StartShopping.js
@@ -1,9 +1,11 @@
 import React from "react";
 import NavBar from "../Navbar";
+import GeneratedShoppingList from "../GeneratedShoppingList";
 
 function StartShopping() {
   return <div className="App">
     <NavBar />
+    <GeneratedShoppingList />
     <h1> Start Shopping Page </h1>
     </div>
 }

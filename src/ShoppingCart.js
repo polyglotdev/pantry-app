1c64a4f2ad86cdb5e9a3ae4ccd03ea07c5cb1f5c
buijennyh <109253137+buijennyh@users.noreply.github.com>
2023-06-01 18:42:14 -0500

    fixing attempt at routing

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -44,12 +44,6 @@ export default function ShoppingCart() {
     return `${month}/${day}/${year}`;
   };
  
-  const history = useHistory();
-  
-  const routeChange = () =>{ 
-    let path = `newPath`; 
-    history.push(path);
-  }
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-4xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">

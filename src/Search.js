51bcede4ac92f1f554c6d313c00f3f728c486977
Robyn <115902450+robynray@users.noreply.github.com>
2023-06-03 18:29:50 -0500

    modified:   src/Search.js

--- src/Search.js
+++ src/Search.js
@@ -119,7 +119,7 @@ export default function Search() {
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                   />
                 </svg>
-                <span className="sr-only">Search</span>
+                <span className="sr-only">Search </span>
               </button>
             </div>
           </div>

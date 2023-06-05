462f9f5777729443fd716c18c63f08c3ca57ab23
redbirdfan <redbirdfan38@gmail.com>
2023-06-04 22:16:32 -0500

    Secrets, secrets are no fun...

--- src/models/recipe.js
+++
@@ -1,5 +0,0 @@
-/*ingredients	string	apples,flour,sugar	A comma-separated list of ingredients that the recipes should contain.
-number	number	10	The maximum number of recipes to return (between 1 and 100). Defaults to 10.
-limitLicense	boolean	true	Whether the recipes should have an open license that allows display with proper attribution.
-ranking	number	1	Whether to maximize used ingredients (1) or minimize missing ingredients (2) first.
-ignorePantry	boolean	true*/

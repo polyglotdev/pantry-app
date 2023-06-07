2205b054bf45f4d711e91b27885ac63a14a80535
Evan Donica <cevandonica@gmail.com>
2023-06-07 14:39:01 -0500

    still not working but logic seems right

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,109 +1,93 @@
-import ExpiringItems from "./Pages/ExpiringItems";
 import axios from 'axios'
 import React, { useEffect, useState } from "react";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import "@splidejs/splide/dist/css/splide.min.css";
 import {Link} from "react-router-dom"
-import { data } from "browserslist";
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+import '@splidejs/splide/dist/css/splide.min.css';
 
 
 export default function Recipe(){
-        const [inventory, setInventory] = useState([]);
-        const [expiringItems, setExpiringItems] = useState([]);
+    const [recipe, setRecipe] = useState([]);
+    const [expiringItems, setExpiringItems] = useState([]);
       
-        useEffect(() => {
-        const fetchInventory = async () => {
-            try {
-            const userID = localStorage.getItem('user');
-    
-            // Make API request with the userID
-            const response = await axios.get(`http://localhost:3001/item/inventory/${userID}`);
-            const inventoryData = response.data;
-    
-            setInventory(inventoryData);
-    
-            const currentDate = new Date();
-            const expiringItems = inventoryData.filter((item) => {
-                const alertDate = new Date(item.alertDate);
-                return alertDate <= currentDate;
-            });
-    
-            setExpiringItems(expiringItems);
-            } catch (error) {
-            console.error('Error fetching inventory:', error);
-            }
-        };
-        fetchInventory();
-}, []);
-      
-  
+    useEffect(() => {
+      const fetchInventory = async () => {
+        try {
+        const userID = localStorage.getItem('user');
 
-useEffect(() => {
-    getRandomRecipes();
-}, []);
+        // Make API request with the userID
+        const response = await axios.get(`http://localhost:3001/item/inventory/${userID}`);
+        const inventoryData = response.data
 
-useEffect(() => {
-    getIngredientRecipes();
-}, [ExpiringItems]);
+        const currentDate = new Date();
+        const expiringItems = inventoryData.filter((item) => {
+        const alertDate = new Date(item.alertDate);
+        return alertDate <= currentDate;
+        });
 
+        setExpiringItems(expiringItems);
+        } catch (error) {
+        console.error('Error fetching inventory:', error);
+        }
+    };
+    fetchInventory();
+  }, []);
 
- const getRandomRecipes = async() => {
-     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=0ae06dc4401d466997fffeb5bdf5ff3d&number=8`);
-     const randomData = await api.json();
-     console.log(randomData);
-     }
-
-const getIngredientRecipes = async() => {
+  const getIngredientRecipes = async() => {
     const ingredientsNames = expiringItems.map((item)=> item.name).join(",");
     console.log(ingredientsNames);
-    const api = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=0ae06dc4401d466997fffeb5bdf5ff3d&ingredients=${ingredientsNames}&number=5`);
+    const api = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=1b432fa04d7a4f608cc1e4ad6491a8c9&ingredients=${ingredientsNames}&number=5`);
     const data = await api.json();
+    setRecipe(data.recipes);
     console.log(data);
     }
- return (
-    <div className="m-0 p-0 box-border bg-white-100"> <h3 className="text-2xl font-bold mb-4 text-gradient">Pantry Fare Farewell: Transforming Expiring Ingredients</h3>
-      <div className=" bg-gray-100 wrapper flex flex-wrap hover:flex-wrap-reverse m-4">
-       
-        {recipe && recipe.length > 0 ? (
-          <Splide
-            options={{
-              perPage: 4,
-              arrows: false,
-              pagination: false,
-              drag: "free",
-              gap: "5rem",
-            }}
-          >
-            {recipe.map((recipe) => (
-              <SplideSlide key={recipe.id}>
-                <div className="min-h-25rem rounded overflow-hidden relative">
-                  <Link to={'/recipedetail/' + recipe.id}>
+
+  useEffect(() => {
+    getIngredientRecipes();
+  }, [expiringItems]);
+  
+  return (
+    <div className="m-0 p-0 box-border bg-white-100">
+    <h3 className="text-2xl font-bold mb-4 text-gradient">Pantry Fare Farewell: Transforming Expiring Ingredients</h3>
+    <div className="bg-gray-100 wrapper flex flex-wrap hover:flex-wrap-reverse m-4">
+      {recipe && recipe.length > 0 ? (
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {recipe.map((recipe) => (
+            <SplideSlide key={recipe.id}>
+              <div className="min-h-25rem rounded overflow-hidden relative">
+                <Link to={'/recipedetail/' + recipe.id}>
                   <div className="absolute z-10 left-0 bottom-0 mb-4 text-center w-full">
                     <p
                       className="text-white text-xl font-semibold"
-                      style={{ 
-                        textShadow: "0px 2px 4px rgba(0, 0, 0, 0.8)" }}
+                      style={{ textShadow: "0px 2px 4px rgba(0, 0, 0, 0.8)" }}
                     >
                       {recipe.title}
                     </p>
-                    
                     <img
                       className="mt-2 rounded-xl w-full h-full object-cover border border-gray-200"
                       src={recipe.image}
                       alt={recipe.title}
                       style={{ borderRadius: "2rem" }}
                     />
-                    </div>
-                    
-                  </Link>
-                </div>
-              </SplideSlide>
-            ))}
-          </Splide>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
+                  </div>
+                </Link>
+              </div>
+            </SplideSlide>
+          ))}
+        </Splide>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  );
-        }
+  </div>
+);
+}
--- src/Veggie.js
+++ src/Veggie.js
@@ -6,72 +6,72 @@ import { Link } from "react-router-dom";
 
     
 function Veggie() {
-    const [veggie, setVeggie] = useState([]);
+    // const [veggie, setVeggie] = useState([]);
   
-    useEffect(() => {
-      getVeggies();
-    }, []);
+    // useEffect(() => {
+    //   getVeggies();
+    // }, []);
   
-    const getVeggies = async () => {
-      // const check = localStorage.getItem('veggie');
+    // const getVeggies = async () => {
+    //   // const check = localStorage.getItem('veggie');
   
-      // if (check) {
-      //   setVeggie(JSON.parse(check));
-      // } else {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=0ebd3d63b0f742c988e9f8f0764797b2&number=9&tags=vegetarian`);
-        const data = await api.json();
-        // localStorage.setItem('veggie', JSON.stringify(data.recipes));
-        setVeggie(data.recipes);
-        console.log(data.recipes);
-      }
+    //   // if (check) {
+    //   //   setVeggie(JSON.parse(check));
+    //   // } else {
+    //     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=0ebd3d63b0f742c988e9f8f0764797b2&number=9&tags=vegetarian`);
+    //     const data = await api.json();
+    //     // localStorage.setItem('veggie', JSON.stringify(data.recipes));
+    //     setVeggie(data.recipes);
+    //     console.log(data.recipes);
+    //   }
     
 
-    return  (
+    // return  (
     
-    <div className="m-0 p-0 box-border bg-white-100"><h3 className="text-2xl font-bold mb-4 text-gradient">Low Stock Dishes </h3>
-    <div className=" bg-gray-100 wrapper flex flex-wrap hover:flex-wrap-reverse m-4">
+    // <div className="m-0 p-0 box-border bg-white-100"><h3 className="text-2xl font-bold mb-4 text-gradient">Low Stock Dishes </h3>
+    // <div className=" bg-gray-100 wrapper flex flex-wrap hover:flex-wrap-reverse m-4">
     
-      {veggie && veggie.length > 0 ? (
-        <Splide
-          options={{
-            perPage: 3,
-            arrows: false,
-            pagination: false,
-            drag: "free",
-            gap: "5rem",
-          }}
-        >
-          {veggie.map((recipe) => (
-            <SplideSlide key={recipe.id}>
-              <div className="min-h-25rem rounded overflow-hidden relative">
-              <Link to={'/recipedetail/'+ recipe.id}>
-                <div className="absolute z-10 left-0 bottom-0 mb-4 text-center w-full">
-                  <p
-                    className="text-white text-xl font-semibold"
-                    style={{
-                      textShadow: "0px 2px 4px rgba(0, 0, 0, 0.8)",
-                    }}
-                  >
-                    {recipe.title}
-                  </p>
-                </div>
-                <img
-                  className="mt-2 rounded-xl w-full h-full object-cover border border-gray-200"
-                  src={recipe.image}
-                  alt={recipe.title}
-                  style={{ borderRadius: "2rem" }}
-                />
-                </Link>
-              </div>
-            </SplideSlide>
-          ))}
-        </Splide>
-                 ) : (
-                <p>Loading...</p>
-            )}
-        </div>
-    </div>
-    )
+    //   {veggie && veggie.length > 0 ? (
+    //     <Splide
+    //       options={{
+    //         perPage: 3,
+    //         arrows: false,
+    //         pagination: false,
+    //         drag: "free",
+    //         gap: "5rem",
+    //       }}
+    //     >
+    //       {veggie.map((recipe) => (
+    //         <SplideSlide key={recipe.id}>
+    //           <div className="min-h-25rem rounded overflow-hidden relative">
+    //           <Link to={'/recipedetail/'+ recipe.id}>
+    //             <div className="absolute z-10 left-0 bottom-0 mb-4 text-center w-full">
+    //               <p
+    //                 className="text-white text-xl font-semibold"
+    //                 style={{
+    //                   textShadow: "0px 2px 4px rgba(0, 0, 0, 0.8)",
+    //                 }}
+    //               >
+    //                 {recipe.title}
+    //               </p>
+    //             </div>
+    //             <img
+    //               className="mt-2 rounded-xl w-full h-full object-cover border border-gray-200"
+    //               src={recipe.image}
+    //               alt={recipe.title}
+    //               style={{ borderRadius: "2rem" }}
+    //             />
+    //             </Link>
+    //           </div>
+    //         </SplideSlide>
+    //       ))}
+    //     </Splide>
+    //              ) : (
+    //             <p>Loading...</p>
+    //         )}
+    //     </div>
+    // </div>
+    // )
 }
 ;
 

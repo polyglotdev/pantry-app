8b70c24124895b41febd073cc69458899722035b
buijennyh <109253137+buijennyh@users.noreply.github.com>
2023-06-06 20:36:49 -0500

    centered sign up page and corrected input boxes

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -21,67 +21,67 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
 export default function SignUpForm() {
     return (
-        <form>
-            <div className="space-y-12 sm:space-y-16">
-                <div>
-                    <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-                    <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
-                        Create a username and password for your LazySusan.
-                    </p>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"> 
+            <form>
+                <div className="space-y-12 sm:space-y-16">
+                    <div>
+                        <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+                        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
+                            Create a username and password for your LazySusan.
+                        </p>
 
-                    <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
-                        <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
-                                Username
-                            </label>
-                            <div className="mt-2 sm:col-span-2 sm:mt-0">
-                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                                    <input
-                                        type="text"
-                                        name="username"
-                                        id="username"
-                                        autoComplete="username"
-                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                        placeholder="janesmith"
-                                    />
+                        <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+                            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                                    Username
+                                </label>
+                                <div className="mt-2 sm:col-span-2 sm:mt-0">
+                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            autoComplete="username"
+                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                                            placeholder="janesmith"
+                                        />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
-                                Password
-                            </label>
-                            <div className="mt-2 sm:col-span-2 sm:mt-0">
-                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                                    <input
-                                        type="text"
-                                        name="password"
-                                        id="password"
-                                        autoComplete="password"
-                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                        placeholder="Your password must be at least 8 characters long."
-                                    
-                                    />
+                            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                                    Password
+                                </label>
+                                <div className="mt-2 sm:col-span-2 sm:mt-0">
+                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                        <input
+                                            type="text"
+                                            name="password"
+                                            id="password"
+                                            autoComplete="password"
+                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                            placeholder="Your password must be at least 8 characters long."
+                                        
+                                        />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                    Cancel
-                </button>
-                <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    Save
-                </button>
-            </div>
-        </form>
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                    <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                        Cancel
+                    </button>
+                    <button
+                        type="submit"
+                        className="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Save
+                    </button>
+                </div>
+            </form> 
+        </div>
     )
 }

ae7e32556f71de7f33dcc5e6e8d1cbf7534ab23c
redbirdfan <redbirdfan38@gmail.com>
2023-06-06 21:23:32 -0500

    expiring ingredients pulling recipes to account for that.  Random also pulling

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,34 +1,55 @@
+import { useState } from "react";
 import { useEffect } from "react";
+import ExpiringItems from "./Pages/ExpiringItems";
 
-//import itemModel from "itemMode../item";
 
-let recipeForExpiring = [];
+export default function Recipe(){
 
-function Recipe(){
-   // if(ItemSchema.methods.isExpiringSoon === true) {
-   //     recipeForExpiring.push(ItemSchema.name)
-    //}
+const [inventory, setInventory] = useState([]);
+const [expiringItems, setExpiringItems] = useState([]);
+ 
+useEffect(() => {
+    const fetchItemInventory = async () => {
+
+      const user = localStorage.getItem('user');
+
+      try {
+        const response = await axios.get(`http://localhost:3001/item/inventory/${user}`);
+        setInventory(response.data)
+        const currentDate = new Date();
+          const expiringItems = inventory.filter((item) => {
+            const alertDate = new Date(item.alertDate);
+            return alertDate <= currentDate;
+            
+        });
+            setExpiringItems(expiringItems)
+            
+                   
+    } catch (error) {
+        console.error('Error fetching inventory:', error);
+        }
+    }
+}, []);
+
 useEffect(() => {
     getRandomRecipes();
 }, []);
 
 useEffect(() => {
     getIngredientRecipes();
-}, []);
+}, [ExpiringItems]);
 
 
 const getRandomRecipes = async() => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=a088ac7df3e54e29b9bc9e5d4e75a7db&number=8`);
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=b547eeba93b64b919c846a7549289de9&number=8`);
     const data = await api.json();
     console.log(data);
     }
 
 const getIngredientRecipes = async() => {
-    const api = await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=a088ac7df3e54e29b9bc9e5d4e75a7db&number=5')
+    const ingredientsNames = expiringItems.map((item)=> item.name).join(",");
+    const api = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=b547eeba93b64b919c846a7549289de9&ingredients=${ingredientsNames}&number=3`);
     const data = await api.json();
     console.log(data);
-}    
-}
-
-
-export default Recipe
+    } 
+} 

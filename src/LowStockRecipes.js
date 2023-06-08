bc7fd3aeb9ffb1b8b95c7a685bd5a736bb5bf051
Evan Donica <cevandonica@gmail.com>
2023-06-07 19:42:10 -0500

    got API calls working

--- src/LowStockRecipes.js
+++ src/LowStockRecipes.js
@@ -2,78 +2,100 @@ import React, { useEffect, useState } from "react";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import "@splidejs/splide/dist/css/splide.min.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-
-    
 function LowStock() {
-    const [lowStock, setLowStock] = useState([]);
-  
-    useEffect(() => {
-      getLowStock();
-    }, []);
-  
-    const getLowStock = async () => {
-      // const check = localStorage.getItem('veggie');
-  
-      // if (check) {
-      //   setVeggie(JSON.parse(check));
-      // } else {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=0ebd3d63b0f742c988e9f8f0764797b2&number=9&tags=vegetarian`);
-        const data = await api.json();
-        // localStorage.setItem('veggie', JSON.stringify(data.recipes));
-        setLowStock(data.recipes);
-        console.log(data.recipes);
+  const [lowStock, setLowStock] = useState([]);
+  const [lowStockItems, setLowStockItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // New loading state
+
+  useEffect(() => {
+    const fetchInventory = async () => {
+      try {
+        const userID = localStorage.getItem("user");
+        const response = await axios.get(`http://localhost:3001/item/inventory/${userID}`);
+        const inventoryData = response.data;
+        const data = inventoryData.filter((item) => item.quantity <= item.minimumQuantity);
+        setLowStockItems(data);
+        setIsLoading(false); // Set loading to false after fetching data
+      } catch (error) {
+        console.error(error);
       }
-    
+    };
 
-    return  (
-    
-    <div className="m-0 p-0 box-border bg-white-100"><h3 className="text-2xl font-bold mb-4 text-gradient">Low Stock Dishes </h3>
-    <div className=" bg-gray-100 wrapper flex flex-wrap hover:flex-wrap-reverse m-4">
-    
-      {lowStock && lowStock.length > 0 ? (
-        <Splide
-          options={{
-            perPage: 3,
-            arrows: false,
-            pagination: false,
-            drag: "free",
-            gap: "5rem",
-          }}
-        >
-          {lowStock.map((recipe) => (
-            <SplideSlide key={recipe.id}>
-              <div className="min-h-25rem rounded overflow-hidden relative">
-              <Link to={'/recipedetail/'+ recipe.id}>
-                <div className="absolute z-10 left-0 bottom-0 mb-4 text-center w-full">
-                  <p
-                    className="text-white text-xl font-semibold"
-                    style={{
-                      textShadow: "0px 2px 4px rgba(0, 0, 0, 0.8)",
-                    }}
-                  >
-                    {recipe.title}
-                  </p>
-                </div>
-                <img
-                  className="mt-2 rounded-xl w-full h-full object-cover border border-gray-200"
-                  src={recipe.image}
-                  alt={recipe.title}
-                  style={{ borderRadius: "2rem" }}
-                />
-                </Link>
-              </div>
-            </SplideSlide>
-          ))}
-        </Splide>
-                 ) : (
-                <p>Loading...</p>
-            )}
-        </div>
+    fetchInventory();
+  }, []);
+
+  useEffect(() => {
+    const fetchLowStockRecipe = async () => {
+      if (lowStockItems.length === 0) {
+        return; // Return early if lowStockItems is empty
+      }
+
+      const ingredientsNames = lowStockItems.map((item) => item.name).join(",");
+      console.log(ingredientsNames);
+      try {
+        const response = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=836fb4ade3ce4eeabbb1e0ee50b43365&ingredients=${ingredientsNames}&number=3`);
+        const data = response.data;
+        console.log(data);
+        setLowStock(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchLowStockRecipe();
+  }, [lowStockItems]);
+
+  return (
+    <div className="m-0 p-0 box-border bg-white-100">
+      <h3 className="text-2xl font-bold mb-4 text-gradient">Low Stock Dishes</h3>
+      <div className="bg-gray-100 wrapper flex flex-wrap hover:flex-wrap-reverse m-4">
+        {!isLoading ? ( // Render only when loading is false
+          lowStock && lowStock.length > 0 ? (
+            <Splide
+              options={{
+                perPage: 3,
+                arrows: false,
+                pagination: false,
+                drag: "free",
+                gap: "5rem",
+              }}
+            >
+              {lowStock.map((recipe) => (
+                <SplideSlide key={recipe.id}>
+                  <div className="min-h-25rem rounded overflow-hidden relative">
+                    <Link to={'/recipedetail/' + recipe.id}>
+                      <div className="absolute z-10 left-0 bottom-0 mb-4 text-center w-full">
+                        <p
+                          className="text-white text-xl font-semibold"
+                          style={{
+                            textShadow: "0px 2px 4px rgba(0, 0, 0, 0.8)",
+                          }}
+                        >
+                          {recipe.title}
+                        </p>
+                      </div>
+                      <img
+                        className="mt-2 rounded-xl w-full h-full object-cover border border-gray-200"
+                        src={recipe.image}
+                        alt={recipe.title}
+                        style={{ borderRadius: "2rem" }}
+                      />
+                    </Link>
+                  </div>
+                </SplideSlide>
+              ))}
+            </Splide>
+          ) : (
+            <p>No low stock dishes available</p>
+          )
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
-    )
+  );
 }
-;
-
 
 export default LowStock;
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,83 +1,106 @@
 import React, { useEffect, useState } from "react";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import "@splidejs/splide/dist/css/splide.min.css";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Recipe() {
   const [recipe, setRecipe] = useState([]);
+  const [expiringItems, setExpiringItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // New loading state
 
   useEffect(() => {
-    getRecipes();
+    const fetchInventory = async () => {
+      try {
+        const userID = localStorage.getItem("user");
+        const response = await axios.get(`http://localhost:3001/item/inventory/${userID}`);
+        const inventoryData = response.data;
+        const currentDate = new Date();
+        const expiringItems = inventoryData.filter((item) => {
+          const alertDate = new Date(item.alertDate);
+          return alertDate <= currentDate;
+        });
+  
+        setExpiringItems(expiringItems);
+        setIsLoading(false); // Set loading to false after fetching data
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchInventory();
   }, []);
 
-  const getRecipes = async () => {
-    // const check = localStorage.getItem('recipe');
+  useEffect(() => {
+    const fetchExpiringRecipe = async () => {
+      if (expiringItems.length === 0) {
+        return; // Return early if expiringItems is empty
+      }
 
-    // if (check) {
-    //   try {
-    //     setRecipe(JSON.parse(check));
-    //   } catch (error) {
-    //     console.error("Error parsing stored recipe data:", error);
-    //     setRecipe([]); 
-    //   }
-    // } else {
-      const api = await fetch(
-        `https://api.spoonacular.com/recipes/random?apiKey=0ebd3d63b0f742c988e9f8f0764797b2&number=5`
-        );
-      const data = await api.json();
-      // localStorage.setItem('recipe', JSON.stringify(data.recipes));
-      setRecipe(data.recipes);
-      console.log(data.recipes);
-    }
-  
+      const ingredientsNames = expiringItems.map((item) => item.name).join(",");
+      console.log(ingredientsNames);
+      try {
+        const response = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=836fb4ade3ce4eeabbb1e0ee50b43365&ingredients=${ingredientsNames}&number=3`);
+        const data = response.data;
+        console.log(data);
+        setRecipe(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchExpiringRecipe();
+  }, [expiringItems]);
 
   return (
-    <div className="m-0 p-0 box-border bg-white-100"> <h3 className="text-2xl font-bold mb-4 text-gradient">Pantry Fare Farewell: Transforming Expiring Ingredients</h3>
-      <div className=" bg-gray-100 wrapper flex flex-wrap hover:flex-wrap-reverse m-4">
-       
-        {recipe && recipe.length > 0 ? (
-          <Splide
-            options={{
-              perPage: 4,
-              arrows: false,
-              pagination: false,
-              drag: "free",
-              gap: "5rem",
-            }}
-          >
-            {recipe.map((recipe) => (
-              <SplideSlide key={recipe.id}>
-                <div className="min-h-25rem rounded overflow-hidden relative">
-                  <Link to={'/recipedetail/' + recipe.id}>
-                  <div className="absolute z-10 left-0 bottom-0 mb-4 text-center w-full">
-                    <p
-                      className="text-white text-xl font-semibold"
-                      style={{ 
-                        textShadow: "0px 2px 4px rgba(0, 0, 0, 0.8)",
-                       }}
-                    >
-                      {recipe.title}
-                    </p>
-                     </div>
-                    <img
-                      className="mt-2 rounded-xl w-full h-full object-cover border border-gray-200"
-                      src={recipe.image}
-                      alt={recipe.title}
-                      style={{ borderRadius: "2rem" }}
-                    />
-                   
-                    
-                  </Link>
-                </div>
-              </SplideSlide>
-            ))}
-          </Splide>
+    <div className="m-0 p-0 box-border bg-white-100">
+      <h3 className="text-2xl font-bold mb-4 text-gradient">Pantry Fare Farewell: Transforming Expiring Ingredients</h3>
+      <div className="bg-gray-100 wrapper flex flex-wrap hover:flex-wrap-reverse m-4">
+        {!isLoading ? ( // Render only when loading is false
+          recipe && recipe.length > 0 ? (
+            <Splide
+              options={{
+                perPage: 3,
+                arrows: false,
+                pagination: false,
+                drag: "free",
+                gap: "5rem",
+              }}
+            >
+              {recipe.map((recipe) => (
+                <SplideSlide key={recipe.id}>
+                  <div className="min-h-25rem rounded overflow-hidden relative">
+                    <Link to={'/recipedetail/' + recipe.id}>
+                      <div className="absolute z-10 left-0 bottom-0 mb-4 text-center w-full">
+                        <p
+                          className="text-white text-xl font-semibold"
+                          style={{
+                            textShadow: "0px 2px 4px rgba(0, 0, 0, 0.8)",
+                          }}
+                        >
+                          {recipe.title}
+                        </p>
+                      </div>
+                      <img
+                        className="mt-2 rounded-xl w-full h-full object-cover border border-gray-200"
+                        src={recipe.image}
+                        alt={recipe.title}
+                        style={{ borderRadius: "2rem" }}
+                      />
+                    </Link>
+                  </div>
+                </SplideSlide>
+              ))}
+            </Splide>
+          ) : (
+            <p>No recipes available</p>
+          )
         ) : (
           <p>Loading...</p>
         )}
       </div>
     </div>
   );
-};
+}
 
 export default Recipe;

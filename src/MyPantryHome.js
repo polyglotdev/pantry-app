93f7eea2e63da3831dfba8014c1cc08421231de4
Robyn <115902450+robynray@users.noreply.github.com>
2023-06-09 10:30:49 -0500

    Update Inventory page

--- src/MyPantryHome.js
+++ src/MyPantryHome.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { BsFillTrashFill, BsFillCartPlusFill } from "react-icons/bs";
+import { BsFillTrashFill, BsFillCartPlusFill,} from "react-icons/bs";
 
 export default function Example() {
   const [pantryItems, setPantryItems] = useState([]);
@@ -30,6 +30,7 @@ export default function Example() {
 
     fetchInventory();
   }, []);
+
 
   const deleteRow = (item) => {
     // Delete logic here
@@ -68,7 +69,7 @@ export default function Example() {
   </tr>
 </thead>
               <tbody>
-                {pantryItems.map((item) => (
+              {pantryItems.map((item) => (
                   <tr key={item._id} className="border-b border-gray-400">
                     <td className="py-2 px-8 text-left">
                       <a href={`/updateitem/${item._id}`} className="text-green-800 hover:text-green-600">
@@ -129,7 +130,7 @@ export default function Example() {
                 </tr>
               </thead>
               <tbody>
-                {refrigeratorItems.map((item) => (
+              {refrigeratorItems.map((item) => (
                   <tr key={item._id} className="border-b border-gray-400">
                     <td className="py-2 px-8 text-left">
                       <a href={`/updateitem/${item._id}`} className="text-green-800 hover:text-green-600">
@@ -179,7 +180,7 @@ export default function Example() {
             <col style={{ width: "20%" }} />
             <col style={{ width: "20%" }} />
             <col style={{ width: "10%" }} />
-</colgroup>
+            </colgroup>
               <thead>
                 <tr>
                   <th className="py-2 px-8 text-left">Name</th>
@@ -191,7 +192,7 @@ export default function Example() {
                 </tr>
               </thead>
               <tbody>
-                {freezerItems.map((item) => (
+              {freezerItems.map((item) => (
                   <tr key={item._id} className="border-b border-gray-400">
                     <td className="py-2 px-8 text-left">
                       <a href={`/updateitem/${item._id}`} className="text-green-800 hover:text-green-600">
@@ -225,256 +226,8 @@ export default function Example() {
               </tbody>
             </table>
           </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import axios from 'axios'
-// import { Fragment, useState, useEffect } from 'react'
-// import { Dialog, Menu, Transition } from '@headlessui/react'
-// import {
-//   ChartBarSquareIcon,
-//   Cog6ToothIcon,
-//   FolderIcon,
-//   GlobeAltIcon,
-//   ServerIcon,
-//   SignalIcon,
-//   XMarkIcon,
-// } from '@heroicons/react/24/outline'
-// import { Bars3Icon, ChevronRightIcon, ChevronUpDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-
-
-//   export default function Example() {
-//     const [pantryItems, setPantryItems] = useState([]);
-//     const [refrigeratorItems, setRefrigeratorItems] = useState([]);
-//     const [freezerItems, setFreezerItems] = useState([]);
-    
-//     useEffect(() => {
-//       const fetchInventory = async () => {
-
-//         const user = localStorage.getItem('user');
-
-//         try {
-//           const response = await axios.get(`http://localhost:3001/item/inventory/${user}`);
-//           const inventoryData = response.data;
-  
-          
-//           const pantryItems = inventoryData.filter((item) => item.location === 'pantry');
-//           setPantryItems(pantryItems);
-  
-//           const refrigeratorItems = inventoryData.filter((item) => item.location === 'refrigerator');
-//           setRefrigeratorItems(refrigeratorItems);
-  
-//           const freezerItems = inventoryData.filter((item) => item.location === 'freezer');
-//           setFreezerItems(freezerItems);
-//         } catch (error) {
-//           console.error('Error fetching inventory:', error);
-//         }
-//       };
-  
-//       fetchInventory();
-//     }, []);
-
-//     return ( 
-//       <div className="flex justify-center">
-//       <div className="h-full ml-14 mr-14 mt-14 mb-10 md:ml-20 md:mr-20 w-screen">
-//       <div>
-//       <br></br>
-//       <h1 className="text-3xl font-bold text-black ">Inventory</h1>
-//       <br></br>
-//       </div>
-//         <div className="mb-8 bg-blue-100 p-4 rounded-lg shadow">
-            
-//                   <h1 className="text-base font-semibold leading-6 text-jet">My Pantry</h1>
-//                   <p className="mt-2 text-sm text-night-300">
-//                     A list of all items in your pantry including their name, quantity, unit, type and expiration date.
-//                   </p>
-                
-//                 {/* <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                  
-//                 </div> */}
-//               <div className="mt-8 flow-root">
-//                 <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-//                   <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-//                     <table className="min-w-full divide-y divide-gray-700">
-//                       <thead>
-//                         <tr>
-//                           <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-night sm:pl-0">
-//                             Name
-//                           </th>
-//                           <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">
-//                             Quantity
-//                           </th>
-//                           <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">
-//                             Unit
-//                           </th>
-//                           <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">
-//                             Food Group
-//                           </th>
-//                           <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">
-//                             Expiration Date
-//                           </th>
-//                           <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-//                             <span className="sr-only">Restock</span>
-//                           </th>
-//                         </tr>
-//                       </thead>
-//                       <tbody className="divide-y divide-gray-800">
-//                         {pantryItems.map((item) => (
-//                           <tr key={item.unit}>
-//                             <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-night sm:pl-0">
-//                             <a href={`/updateitem/${item._id}`} className="text-green-800 hover:text-green-600">
-//                               {item.name}
-//                             </a>
-//                             </td>
-//                             <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.quantity}</td>
-//                             <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.unit}</td>
-//                             <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.foodGroup}</td>
-//                             <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{new Date(item.expirationDate).toLocaleDateString('en-US')}</td>
-//                             <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-//                             <a href="#" className="text-green-800 hover:text-green-600">
-//                                 Add to Shopping List<span className="sr-only">, {item.name}</span>
-//                               </a>
-//                             </td>
-//                           </tr>
-//                         ))}
-//                       </tbody>
-//                     </table>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           {/* <div className="mx-auto max-w-7xl"> */}
-//             <div className="mb-8 bg-blue-100 p-4 rounded-lg shadow">
-//                     <h1 className="text-base font-semibold leading-6 text-jet">My
-//                       Refrigerator</h1>
-//                     <p className="mt-2 text-sm text-night-300">
-//                       A list of all items in my refrigerator including their name, quantity, unit, type and expiration date.
-//                     </p>
-//               <div className="mt-8 flow-root">
-//                 <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-//                   <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-//                     <table className="min-w-full divide-y divide-gray-700">
-//                       <thead>
-//                         <tr>
-//                           <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-night sm:pl-0">
-//                             Name
-//                           </th>
-//                           <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">
-//                             Quantity
-//                           </th>
-//                           <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">
-//                             Unit
-//                           </th>
-//                           <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">
-//                             Food Type
-//                           </th>
-//                           <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">
-//                             Expiration Date
-//                           </th>
-//                           <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-//                             <span className="sr-only">Restock</span>
-//                           </th>
-//                         </tr>
-//                       </thead>
-//                       <tbody className="divide-y divide-gray-800">
-//                         {refrigeratorItems.map((item) => (
-//                           <tr key={item.unit}>
-//                             <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-night sm:pl-0">
-//                             <a href={`/updateitem/${item._id}`} className="text-green-800 hover:text-green-600">
-//                               {item.name}
-//                             </a>
-//                             </td>
-//                             <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.quantity}</td>
-//                             <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.unit}</td>
-//                             <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.foodGroup}</td>
-//                             <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{new Date(item.expirationDate).toLocaleDateString('en-US')}</td>
-//                             <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-//                               <a href="#" className="text-green-800 hover:text-green-600">
-//                                 Add to Shopping List<span className="sr-only">, {item.name}</span>
-//                               </a>
-//                             </td>
-//                           </tr>
-//                         ))}
-//                       </tbody>
-//                     </table>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           {/* </div> */}
-        
-     
-//             <div className="mb-8 bg-blue-100 p-4 rounded-lg shadow">
-//                     <h1 className="text-base font-semibold leading-6 text-night">My Freezer</h1>
-//                     <p className="mt-2 text-sm text-night-300">
-//                       A list of all items in my freezer including their name, quantity, unit, type and expiration date.
-//                     </p>
-//                 <div className="mt-8 flow-root">
-//                   <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-//                     <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-//                       <table className="min-w-full divide-y divide-gray-700">
-//                         <thead>
-//                           <tr>
-//                             <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-night sm:pl-0">Name</th>
-//                             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">Quantity</th>
-//                             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">Unit</th>
-//                             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">Food Type</th>
-//                             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-night">Expiration Date</th>
-//                             <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-//                               <span className="sr-only">Restock</span>
-//                             </th>
-//                           </tr>
-//                         </thead>
-//                         <tbody className="divide-y divide-gray-800">
-//                           {freezerItems.map((item) => (
-//                             <tr key={item.unit}>
-//                               <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-night sm:pl-0">
-//                               <a href={`/updateitem/${item._id}`} className="text-green-800 hover:text-green-600">
-//                                 {item.name}
-//                               </a>
-//                               </td>
-//                               <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.quantity}</td>
-//                               <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.unit}</td>
-//                               <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{item.foodGroup}</td>
-//                               <td className="whitespace-nowrap px-3 py-4 text-sm text-night-300">{new Date(item.expirationDate).toLocaleDateString('en-US')}</td>
-//                               <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-//                                 <a href="#" className="text-green-800 hover:text-green-700">
-//                                  Add to Shopping List<span className="sr-only">, {item.name}</span>
-//                                 </a>
-//                               </td>
-//                             </tr>
-//                           ))}
-//                         </tbody>
-//                       </table>
-//                     </div>
-//                   </div>
-//                 </div>    
-//             </div>
-//       </div>
-//     </div>
-//     )
-//   }

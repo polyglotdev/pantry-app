217a045707e953f4c2ff349d17686918502ac43b
Evan Donica <cevandonica@gmail.com>
2023-06-09 21:16:24 -0500

    user is directed to about page if not logged in

--- src/App.js
+++ src/App.js
@@ -1,55 +1,61 @@
-import React from 'react'
+import React from 'react';
 import './index.css';
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import SignUpPage from './Pages/SignUpPage'
-import Pantry from './Pages/Pantry'
-import ShoppingList from './Pages/ShoppingList'
-import LoginPage from './Pages/LoginPage'
-import ItemForm from './Pages/CreateItem'
-import Update from './Pages/EditItem'
-import SearchPage from './Pages/SearchPage'
-import StartShopping from './Pages/StartShopping'
-import RecipePage from './Pages/RecipePage'
-import ExpiringItems from './Pages/ExpiringItems'
-import Settings from './Pages/Settings'
-import SignedOut from './Pages/SignedOut'
+import SignUpPage from './Pages/SignUpPage';
+import Pantry from './Pages/Pantry';
+import ShoppingList from './Pages/ShoppingList';
+import LoginPage from './Pages/LoginPage';
+import ItemForm from './Pages/CreateItem';
+import Update from './Pages/EditItem';
+import SearchPage from './Pages/SearchPage';
+import StartShopping from './Pages/StartShopping';
+import RecipePage from './Pages/RecipePage';
+import ExpiringItems from './Pages/ExpiringItems';
+import Settings from './Pages/Settings';
+import SignedOut from './Pages/SignedOut';
 import Home from './Pages/Home';
 import LazySusan from './Pages/LazySusan';
 import Cuisine from './Pages/Cuisine';
 import RecipeSearched from './Pages/RecipeSearched';
 import RecipeDetail from './Pages/RecipeDetail';
 
-
-
+// Simulating the user login status
+const isAuthenticated = localStorage.getItem('user');
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/lazysusan" element={<LazySusan />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/inventory" element={<Pantry />} />
-        <Route path="/shoppinglist" element={<ShoppingList />} />
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/itemform" element={<ItemForm />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/startshopping" element={<StartShopping />} />
-        <Route path="/recipes" element={<RecipePage />} />
-        <Route path="/updateitem/:itemId" element={<Update/>} />
-        <Route path="/expiringitems" element ={<ExpiringItems />} />
-        <Route path="/settings" element ={<Settings />} />
-        <Route path="/signedout" element ={<SignedOut />} />
-        <Route path="/home" element ={<Home />} />
-        <Route path="/cuisine/:type" element ={<Cuisine />} />
-        <Route path="/recipesearched/:recipesearch" element ={<RecipeSearched />} />
-        <Route path="/recipedetail/:name" element ={<RecipeDetail />} />
-
+        <Route path="/login" element={<LoginPage />} />
+        {/* Routes that require authentication */}
+        {isAuthenticated && (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/inventory" element={<Pantry />} />
+            <Route path="/shoppinglist" element={<ShoppingList />} />
+            <Route path="/itemform" element={<ItemForm />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/startshopping" element={<StartShopping />} />
+            <Route path="/recipes" element={<RecipePage />} />
+            <Route path="/updateitem/:itemId" element={<Update />} />
+            <Route path="/expiringitems" element={<ExpiringItems />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/signedout" element={<SignedOut />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cuisine/:type" element={<Cuisine />} />
+            <Route path="/recipesearched/:recipesearch" element={<RecipeSearched />} />
+            <Route path="/recipedetail/:name" element={<RecipeDetail />} />
+          </>
+        )}
+        {/* Redirect to /lazysusan if not logged in */}
+        {!isAuthenticated && <Route path="*" element={<Navigate to="/lazysusan" replace />} />}
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -63,6 +63,8 @@ export default function SignUpForm() {
                                             autoComplete="username"
                                             className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                             placeholder="janesmith"
+                                            value={username}
+                                            onChange={(e) => setUsername(e.target.value)}
                                         />
                                     </div>
                                 </div>
@@ -80,7 +82,8 @@ export default function SignUpForm() {
                                             autoComplete="password"
                                             className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                             placeholder="Your password must be at least 8 characters long."
-                                        
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
                                         />
                                     </div>
                                 </div>
@@ -96,6 +99,7 @@ export default function SignUpForm() {
                     <button
                         type="submit"
                         className="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        onClick={handleSubmit}
                     >
                         Save
                     </button>

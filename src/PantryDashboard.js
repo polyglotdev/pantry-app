6e4c55b52af0a29852d1f95c87e9a7d0c26fc19a
Evan Donica <cevandonica@gmail.com>
2023-06-11 18:11:01 -0500

    fixed dashboard

--- src/PantryDashboard.js
+++ src/PantryDashboard.js
@@ -182,7 +182,7 @@ export default function Dashboard() {
                   key={item.id}
                   className="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer"
                 >
-                <a href={`/updateitem/${item._id}`} className="text-gray-600">
+                <a href= '#' onClick={() => openEditModal(item._id)} className="text-gray-600">
                   {item.name}
                 </a>
                   {expiringItems.includes(item) && (
@@ -223,7 +223,7 @@ export default function Dashboard() {
                   key={item.id}
                   className="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer"
                 >
-                <a href={`/updateitem/${item._id}`} className="text-gray-600">
+                <a href= '#' onClick={() => openEditModal(item._id)} className="text-gray-600">
                   {item.name}
                 </a>
                   {expiringItems.includes(item) && (

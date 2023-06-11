b62ce8cb7cd5493689610d98f8310a0c89068518
Evan Donica <cevandonica@gmail.com>
2023-06-11 15:13:38 -0500

    added logic to filter expiring and low stock items

--- src/ExpirationAndLowStock.js
+++ src/ExpirationAndLowStock.js
@@ -7,8 +7,8 @@ import { BsFillTrashFill, BsFillCartPlusFill,BsFillPencilFill} from "react-icons
 import EditItem from "./EditItem"
 
 export default function ExpirationAndLowStock() {
-  const [pantryItems, setPantryItems] = useState([]);
-  const [refrigeratorItems, setRefrigeratorItems] = useState([]);
+  const [expiringItems, setExpiringItems] = useState([]);
+  const [lowsStockItems, setLowStockItems] = useState([]);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false)
   const [editItemId, setEditItemId] = useState(null);
 
@@ -23,11 +23,11 @@ export default function ExpirationAndLowStock() {
         const response = await axios.get(`http://localhost:3001/item/inventory/${user}`);
         const inventoryData = response.data;
 
-        const pantryItems = inventoryData.filter((item) => item.location === "pantry");
-        setPantryItems(pantryItems);
+        const expiring = inventoryData.filter((item) => item.restock);
+        setExpiringItems(expiring);
 
-        const refrigeratorItems = inventoryData.filter((item) => item.location === "refrigerator");
-        setRefrigeratorItems(refrigeratorItems);
+        const lowStock = inventoryData.filter((item) => item.quantity <= item.minimumQuantity);
+        setLowStockItems(lowStock);
 
        
       } catch (error) {
@@ -39,20 +39,12 @@ export default function ExpirationAndLowStock() {
   }, []);
 
 
-  const deleteRow = (itemID, location) => {
+  const deleteRow = (itemID) => {
     const confirmed = window.confirm('Are you sure you want to delete this item?');
     if (confirmed) {
       axios.delete(`http://localhost:3001/item/${itemID}`);
-      if (location === "pantry") {
-        setPantryItems(pantryItems.filter((item) => item._id !== itemID));
-      }
-      if (location === "refrigerator") {
-        setRefrigeratorItems(refrigeratorItems.filter((item) => item._id !== itemID));
-      }
-      if (location === "freezer") {
-        setFreezerItems(freezerItems.filter((item) => item._id !== itemID));
-      }
   }
+  window.location.reload();
   };
 
   const shopRow = (itemID) => {
@@ -101,7 +93,7 @@ export default function ExpirationAndLowStock() {
   </tr>
 </thead>
               <tbody>
-              {pantryItems.map((item) => (
+              {expiringItems.map((item) => (
                   <tr key={item._id} className="border-b border-gray-400">
                     <td className="py-2 px-8 text-left">
                       {/* <a href={`/updateitem/${item._id}`} className="text-green-800 hover:text-green-600"> */}
@@ -127,7 +119,7 @@ export default function ExpirationAndLowStock() {
                       />
                       <BsFillTrashFill
                         className="flex flex-col justify-center items-center rounded-full p-1 m-1 text-center text-white bg-gradient-to-br from-gray-900 to-gray-700 w-6 h-6 cursor-pointer transform scale-80 hover:scale-100 hover:bg-gradient-to-br hover:from-gray-900 hover:to-gray-600 hover:ring-2 hover:ring-green-500 transition-all duration-300"
-                        onClick={() => deleteRow(item._id, item.location)}
+                        onClick={() => deleteRow(item._id)}
                       />
                       <BsFillCartPlusFill
                         className="flex flex-col justify-center items-center rounded-full p-1 m-1 text-center text-white bg-gradient-to-br from-gray-900 to-gray-700 w-6 h-6 cursor-pointer transform scale-80 hover:scale-100 hover:bg-gradient-to-br hover:from-gray-900 hover:to-gray-600 hover:ring-2 hover:ring-green-500 transition-all duration-300"
@@ -166,7 +158,7 @@ export default function ExpirationAndLowStock() {
                 </tr>
               </thead>
               <tbody>
-              {refrigeratorItems.map((item) => (
+              {lowsStockItems.map((item) => (
                   <tr key={item._id} className="border-b border-gray-400">
                     <td className="py-2 px-8 text-left">
                       {/* <a href={`/updateitem/${item._id}`} className="text-green-800 hover:text-green-600"> */}
@@ -192,7 +184,7 @@ export default function ExpirationAndLowStock() {
                       />
                       <BsFillTrashFill
                         className="flex flex-col justify-center items-center rounded-full p-1 m-1 text-center text-white bg-gradient-to-br from-gray-900 to-gray-700 w-6 h-6 cursor-pointer transform scale-80 hover:scale-100 hover:bg-gradient-to-br hover:from-gray-900 hover:to-gray-600 hover:ring-2 hover:ring-green-500 transition-all duration-300"
-                        onClick={() => deleteRow(item._id, item.location)}
+                        onClick={() => deleteRow(item._id)}
                       />
                       <BsFillCartPlusFill
                         className="flex flex-col justify-center items-center rounded-full p-1 m-1 text-center text-white bg-gradient-to-br from-gray-900 to-gray-700 w-6 h-6 cursor-pointer transform scale-80 hover:scale-100 hover:bg-gradient-to-br hover:from-gray-900 hover:to-gray-600 hover:ring-2 hover:ring-green-500 transition-all duration-300"
